1607fbaa2d55f699aad12e67a1d60dc722780a0b
Robert Mafort <rbrmafort@gmail.com>
2020-09-20 12:54:31 -0300

    Front-mobile production ready

--- front-mobile/src/pages/CreateRecord/index.tsx
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -12,7 +12,7 @@ const placeholder = {
 	label: 'Selecione o game',
 	value: null,
 };
-const BASE_URL = 'http://192.168.1.105:8080';
+const BASE_URL = 'https://sds1-robert.herokuapp.com';
 
 const mapSelectValues = (games: Game[]) => {
 	return games.map((game) => ({
